999d3530852cc02b9bfd4289b81aee2e3185788f
Johnny <johnkcli@hotmail.com>
2023-07-18 11:59:22 +0100

    Revert "refresh room message notifications after deleting message"
    This reverts commit 20f473108c7d7b18f8e6949ea15cbef238f93af7.

--- blabhear/admin.py
+++ blabhear/admin.py
@@ -1,5 +1,3 @@
-from asgiref.sync import async_to_sync
-from channels.layers import get_channel_layer
 from django.contrib import admin
 
 from blabhear.models import Report, Message
@@ -11,22 +9,6 @@ class ReadOnlyReportModelAdmin(admin.ModelAdmin):
 
 class ReadOnlyMessageModelAdmin(admin.ModelAdmin):
     readonly_fields = ("creator", "room")
-
-    def delete_model(self, request, obj):
-        super().delete_model(request, obj)
-        room_id = obj.room.id
-        channel_layer = get_channel_layer()
-        async_to_sync(channel_layer.group_send)(
-            str(room_id), {"type": "refresh_notifications"}
-        )
-
-    def delete_queryset(self, request, queryset):
-        super().delete_queryset(request, queryset)
-        for room_id in list(queryset.order_by().values_list("room__id").distinct()):
-            channel_layer = get_channel_layer()
-            async_to_sync(channel_layer.group_send)(
-                str(room_id), {"type": "refresh_notifications"}
-            )
 
 
 admin.site.register(Report, ReadOnlyReportModelAdmin)
