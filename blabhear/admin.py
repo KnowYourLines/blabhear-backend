b86e648bd695bf5889ab2f0f08e709af5d582c32
Johnny <johnkcli@hotmail.com>
2023-08-14 04:28:44 +0100

    paginated message notifications

--- blabhear/admin.py
+++ blabhear/admin.py
@@ -4,11 +4,13 @@ from blabhear.models import Report, Message
 
 
 class ReadOnlyReportModelAdmin(admin.ModelAdmin):
-    readonly_fields = ("reporter", "reported_user", "message")
+    readonly_fields = ("reporter", "reported_user", "message", "reported_at")
+    list_display = ("reporter", "reported_user", "message", "reported_at")
 
 
 class ReadOnlyMessageModelAdmin(admin.ModelAdmin):
-    readonly_fields = ("creator", "room")
+    readonly_fields = ("creator", "room", "created_at")
+    list_display = ("creator", "room", "created_at")
 
 
 admin.site.register(Report, ReadOnlyReportModelAdmin)
